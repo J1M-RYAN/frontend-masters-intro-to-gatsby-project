6fda067d28999c389207d400d08825b111cd7bd0
Jim Ryan <jimmih15@gmail.com>
2021-10-24 15:21:11 +0100

    links

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import { Link } from 'gatsby';
+import * as React from 'react';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About this site</h1>
+      <Link to="/">Go home</Link>
+    </main>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Hello</h1>
+      <Link to="/about">About</Link>
+    </main>
+  );
+};
+
+export default IndexPage;
