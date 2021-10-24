e4e850c6dd223f6bdcc86d9081e04521b8c1ac67
Jim Ryan <jimmih15@gmail.com>
2021-10-24 16:44:56 +0100

    seo

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://somedomain.com',
+    title: 'Frontend masters intro to gatsby',
+    description: 'frontend masters course',
+    image: 'https://api.corgi.cam/v1/img/',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Seo from '../components/seo.js';
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const meta = data?.site?.siteMetadata ?? {};
   return (
-    <main>
-      <h1>Hello</h1>
-      <Link to="/about">About</Link>
-    </main>
+    <>
+      <Seo />
+      <header>
+        <Link to="/">{meta.title}</Link>
+      </header>
+      <main>
+        <h1>Hello</h1>
+        <Link to="/about">About</Link>
+      </main>
+    </>
   );
 };
 
